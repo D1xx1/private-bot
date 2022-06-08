7f3ac6b604f1cd59e6ef28b0770292add093dcb7
Viktor <95516464+D1xx1@users.noreply.github.com>
2022-06-09 02:37:56 +0700

    Create keyboards.py

---
+++ Keyboards/keyboards.py
@@ -0,0 +1,23 @@
+from telebot import types
+
+class RuKeyboard():
+    def MainMenuKeyboard():
+        Keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+        Button1 = types.KeyboardButton('Поиск картинок')
+        Button2 = types.KeyboardButton('Сменить язык')
+        Button3 = types.KeyboardButton('Статус')
+        Keyboard.add(Button1, Button2, Button3)
+        return Keyboard
+    
+    def backButton():
+        Keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        Button1 = types.KeyboardButton('Назад')
+        Keyboard.add(Button1)
+        return Keyboard
+
+    def adminKeyboard():
+        Keyboard = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
+        Button1 = types.KeyboardButton('Добавить')
+        Button2 = types.KeyboardButton('Назад')
+        Keyboard.add(Button1,Button2)
+        return Keyboard
