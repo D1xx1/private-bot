bc92f91a8a8fa034386b2d92be7091e76eb816db
Viktor <95516464+D1xx1@users.noreply.github.com>
2022-06-09 02:40:51 +0700

    Update keyboards.py

--- Keyboards/keyboards.py
+++ Keyboards/keyboards.py
@@ -21,3 +21,24 @@ class RuKeyboard():
         Button2 = types.KeyboardButton('Назад')
         Keyboard.add(Button1,Button2)
         return Keyboard
+
+class EnKeyboard():
+    def MainMenuKeyboard():
+        Keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+        MenuButton1 = types.KeyboardButton('Find picture')
+        MenuButton2 = types.KeyboardButton('Change language')
+        Keyboard.add(MenuButton1, MenuButton2)
+        return Keyboard
+
+    def backButton():
+        Keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        MenuButton1 = types.KeyboardButton('Back')
+        Keyboard.add(MenuButton1)
+        return Keyboard
+
+    def adminKeyboard():
+        Keyboard = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
+        Button1 = types.KeyboardButton('Add')
+        Button2 = types.KeyboardButton('Back')
+        Keyboard.add(Button1,Button2)
+        return Keyboard
