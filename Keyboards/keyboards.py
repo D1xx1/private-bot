8aeced89e9d53ce85580583a9017a20362366695
Viktor <95516464+D1xx1@users.noreply.github.com>
2022-06-09 02:48:59 +0700

    Update keyboards.py

--- Keyboards/keyboards.py
+++ Keyboards/keyboards.py
@@ -42,3 +42,15 @@ class EnKeyboard():
         Button2 = types.KeyboardButton('Back')
         Keyboard.add(Button1,Button2)
         return Keyboard
+
+class StartKeyboard():
+    def startKeyboard():
+        Keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
+        Button1 = types.KeyboardButton('Русский',)
+        Button2 = types.KeyboardButton('English')
+        Keyboard.add(Button1, Button2)
+        return Keyboard
+
+    def removeKeyboard():
+        Keyboard = types.ReplyKeyboardRemove()
+        return Keyboard
